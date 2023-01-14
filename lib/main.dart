02d67c8579a8c358c28a9ca6da2cf27301d3b389
The Vedic Developer <thevedicdev@gmail.com>
2023-01-14 15:30:45 -0500

    Day 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:codepur_30_days_of_flutter/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,13 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 Days of Flutter!"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
