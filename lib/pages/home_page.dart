9895f9dd4561c2ee5172eb5ecfeb6a27eed0ea53
The Vedic Developer <thevedicdev@gmail.com>
2023-01-14 16:26:34 -0500

    day 3 phase 1

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int days = 15;
+  final String name = "Codepur";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days Days of Flutter! by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
